0834f103087a5ae61fbacb0cff90b192fcb72c22
anntnzrb <anntnzrb@protonmail.com>
2023-07-13 05:41:41 +0000

    init

---
+++ src/l217.rs
@@ -0,0 +1,27 @@
+use std::collections::HashSet;
+use std::iter::FromIterator;
+
+/// Sets contain unique elements only.
+/// (element cannot be present more than once)
+///
+/// Making a Set from a collection (which may have repeated elements)
+/// leads to a new Set with the collection's unique elements.
+///
+/// If the sizes of both collection and set differ, then the collection had
+/// repeated elements.
+
+pub fn contains_duplicate(nums: Vec<i32>) -> bool {
+    nums.len() != HashSet::<i32>::from_iter(nums).len()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn solution_v1() {
+        assert_eq!(contains_duplicate(vec![1, 2, 3, 1]), true);
+        assert_eq!(contains_duplicate(vec![1, 2, 3, 4]), false);
+        assert_eq!(contains_duplicate(vec![1, 1, 1, 3, 3, 4, 3, 2, 4, 2]), true);
+    }
+}
